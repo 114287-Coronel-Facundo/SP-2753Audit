fea4d291050b5cb19b95cac99db135f904b3eeb5
facuCoronel <92610450+facuCoronel@users.noreply.github.com>
2024-07-10 15:15:05 -0300

    add audit

---
+++ EvoltisTL.AuditDomain/EvoltisTL.AuditDomain/AuditableEntitySavingException.cs
@@ -0,0 +1,8 @@
+﻿namespace EvoltisTL.AuditDomain
+{
+    public class AuditableEntitySavingException : Exception
+    {
+        public AuditableEntitySavingException(IEnumerable<string> entityTypeNames) : base($"Entit{(entityTypeNames.Count() == 1 ? "y" : "ies")} of the type: {entityTypeNames.Aggregate(string.Empty, (actual, next) => actual + ", " + next )} {(entityTypeNames.Count() == 1 ? "is" : "are")} Auditable entities need the user id to be saved, add UserId to call the method")
+        { }
+    }
+}
---
+++ EvoltisTL.AuditDomain/EvoltisTL.AuditDomain/EvoltisAuditServicesConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoltisTL.AuditDomain
+{
+    public static class EvoltisAuditServicesConfiguration
+    {
+        public static IServiceCollection ConfigureAudit(this IServiceCollection services, string connectionString)
+        {
+
+            services.AddDbContext<AuditDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+            //services.AddAutoMapper((new BaseModel()).GetType().Assembly);
+
+            return services;
+        }
+    }
+}
