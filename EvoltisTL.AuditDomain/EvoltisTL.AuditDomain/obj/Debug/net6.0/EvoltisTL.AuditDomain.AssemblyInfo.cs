d5f1fe945c482b1ecd51e8110d5a114319e00267
facuCoronel <92610450+facuCoronel@users.noreply.github.com>
2024-07-19 15:08:50 -0300

    healt check

--- EvoltisTL.AuditDomain/EvoltisTL.AuditDomain/obj/Debug/net6.0/EvoltisTL.AuditDomain.AssemblyInfo.cs
+++ EvoltisTL.AuditDomain/EvoltisTL.AuditDomain/obj/Debug/net6.0/EvoltisTL.AuditDomain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvoltisTL.AuditDomain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e89d03ccc0e1e6ccca6098f2efd07f98188f15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcfe4467600d4dd921fe471573ff8882ec683261")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvoltisTL.AuditDomain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvoltisTL.AuditDomain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
